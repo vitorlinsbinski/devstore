4d6f8233c7a34780c66e8a0b617cfec22f17e144
vitorlinsbinski <vitor.linsbinski@hotmail.com>
2024-01-11 16:55:17 -0400

    Add: home page, product page, product api

--- src/app/(store)/product/[slug]/loading.tsx
+++ src/app/(store)/product/[slug]/loading.tsx
@@ -1,10 +1,9 @@
 import { Skeleton } from "@/components/skeleton";
 
-export default function HomeLoading() {
+export default function ProductLoading() {
   return (
     <div className="grid h-full grid-cols-9 grid-rows-6 gap-6">
       <Skeleton className="col-span-6 row-span-6 h-[856px]" />
-      <Skeleton className="col-span-3 row-span-3" />
       <Skeleton className="col-span-3 row-span-3" />
     </div>
   );

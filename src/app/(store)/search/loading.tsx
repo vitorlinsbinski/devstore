fc26da9276aa36a3bca63feb76fc91b18ebadebe
vitorlinsbinski <vitor.linsbinski@hotmail.com>
2024-01-15 11:28:54 -0400

    Add: cypress config and initial e2e tests

--- src/app/(store)/search/loading.tsx
+++ src/app/(store)/search/loading.tsx
@@ -1,3 +1,21 @@
+import { Skeleton } from "@/components/skeleton";
+import { Suspense } from "react";
+import { CurrentSearch } from "./current-search";
 export default function SearchLoading() {
-  return <span>Carregando busca...</span>;
+  return (
+    <div className="flex flex-col gap-4">
+      <Suspense fallback={null}>
+        <CurrentSearch />
+      </Suspense>
+
+      <div className="grid grid-cols-3 gap-6">
+        <Skeleton className="h-[462px]" />
+        <Skeleton className="h-[462px]" />
+        <Skeleton className="h-[462px]" />
+        <Skeleton className="h-[462px]" />
+        <Skeleton className="h-[462px]" />
+        <Skeleton className="h-[462px]" />
+      </div>
+    </div>
+  );
 }
